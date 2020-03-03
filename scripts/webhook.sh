82697a5affac91e8fc9e61ac968487dff3e83607
Aliaksei Sabetski <ausard@yandex.ru>
2020-03-03 23:29:11 +0300

    ls

--- scripts/webhook.sh
+++ scripts/webhook.sh
@@ -6,4 +6,4 @@ chmod +wx /usr/local/bin/relay
 export RELAY_KEY=7b3e24c2-8711-4930-aaca-4cdaa2d3fa5c
 export RELAY_SECRET=O12ZmWw3VrLL
 
-relay forward --bucket jenkins http://localhost:8080/github-webhook/ > /den/null &
+relay forward --bucket jenkins http://localhost:8080/github-webhook/ >> /tmp/webhook.log &
